a6f0b5e5d363bf4aced8a2dd7edcc7d9c4d2a4d1
MiguelEWV <miguelwellspay1@gmail.com>
2021-09-08 16:51:51 -0300

    Algunos cambis aplicativo funcionando pero las funciones del menu todavia

--- app/src/main/java/com/miguelewv/menudesplegable/MainActivity.java
+++ app/src/main/java/com/miguelewv/menudesplegable/MainActivity.java
@@ -1,15 +1,27 @@
 package com.miguelewv.menudesplegable;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity
+
+    implements NavigationView.OnNavigationItemSelectedListener, Persona1Fragment.OnFragmentInterationListener
+
+{
 
 
 
@@ -21,18 +33,51 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+//Funcionalidad para que regrese o se cierre el menu drawer despues de dar en el boton de atras=================
+    @Override public void onBackPressed(){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.elprincipal);
+        if (drawer.isDrawerOpen(GravityCompat.START)){drawer.closeDrawer(GravityCompat.START);
+        }else{
+        super.onBackPressed();
+        }
+    }
+//=============================================================================================================
 
     public boolean onNavigationItemSelected (MenuItem item){
         int id = item.getItemId();
+
+        boolean FragmentTransantion = false;
+       // Fragment fragmentpersona1 = null;
         FragmentManager fm = getSupportFragmentManager(); //llamada del suporte del gestor de fragment
-        if (id == R.id.idmenupersona1){
-            Toast.makeText(this, "Persona 1", Toast.LENGTH_SHORT).show();
-            fm.beginTransaction().replace(R.id.elprincipal,new Persona1Fragment()).commit();//opcion para hacer la transferencia
-        } else if (id == R.id.idmenupersona2){
-            fm.beginTransaction().replace(R.id.fragmentContainerView,new Persona2Fragment()).commit();
+
+
+         if (id == R.id.idmenupersona1){
+             Fragment fragmentpersona1 = new Persona1Fragment();// Llama por las importaciones el fragment persona (Ojo el tipo de importacion deve ser igual en el mainactivity como en el fragment "import androidx.fragment.app.Fragment;")
+             FragmentTransantion = true;
+
+             Toast.makeText(this, "Persona 1", Toast.LENGTH_SHORT).show();
+            fm.beginTransaction().replace(R.id.fragmentContainerView,new Persona1Fragment()).commit();//opcion para hacer la transferencia
         }
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.persona2Fragment);
+        else
+            if (id == R.id.idmenupersona2){
+                Fragment fragmentpersona2 = new Persona2Fragment();// Llama por las importaciones el fragment persona (Ojo el tipo de importacion deve ser igual en el mainactivity como en el fragment "import androidx.fragment.app.Fragment;")
+                FragmentTransantion = true;
+
+
+                
+            Toast.makeText(this, "Persona 2", Toast.LENGTH_SHORT).show();
+            fm.beginTransaction().replace(R.id.elprincipal,new Persona2Fragment()).commit();
+        }
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.elprincipal);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-}
+
+
+   
+
+
+    }
+
--- app/src/main/java/com/miguelewv/menudesplegable/Persona1Fragment.java
+++ app/src/main/java/com/miguelewv/menudesplegable/Persona1Fragment.java
@@ -1,9 +1,7 @@
 package com.miguelewv.menudesplegable;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,5 +13,9 @@ public class Persona1Fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_persona1, container, false);
+    }
+
+
+    public interface OnFragmentInterationListener {
     }
 }
