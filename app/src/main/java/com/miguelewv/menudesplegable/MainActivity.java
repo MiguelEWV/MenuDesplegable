77a512b8a72c8df3e7257259a36925dcd6325eaa
MiguelEWV <miguelwellspay1@gmail.com>
2021-09-09 14:50:22 -0300

    Algunos cambis aplicativo funcionando con las funciones del menu mostrando Toast

--- app/src/main/java/com/miguelewv/menudesplegable/MainActivity.java
+++ app/src/main/java/com/miguelewv/menudesplegable/MainActivity.java
@@ -1,83 +1,63 @@
 package com.miguelewv.menudesplegable;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.os.Bundle;
 import android.view.MenuItem;
-
-import android.widget.Button;
 import android.widget.Toast;
-
 import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity {
 
-    implements NavigationView.OnNavigationItemSelectedListener, Persona1Fragment.OnFragmentInterationListener
-//OJO 19. Navigation Drawer. (Programación Android Studio tutorial español) minuto 35
-{
-
-
-
+//===========================================================  Declaracion de variables  ===================================================================
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView ;
+ //==========================================================================================================================================================
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//===================================================== LLamada del navigation view y el drawer Layout ======================================================
+        drawerLayout = findViewById(R.id.idDrawerLayout);
+        navigationView = findViewById(R.id.idNavigationView);
 
-
-
+//==========================================================================================================================================================
+//=========================================== Accion de los botones del NavigationView dentro del DrawerLayout =============================================
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case R.id.idmenupersona1:
+                        Toast.makeText(MainActivity.this, "Persona 1 clicado",Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.idmenupersona2:
+                        Toast.makeText(MainActivity.this, "Persona 2 clicado",Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return true;
+            }
+        });
+//===========================================================================================================================================================
     }
-//Funcionalidad para que regrese o se cierre el menu drawer despues de dar en el boton de atras=================
-    @Override public void onBackPressed(){
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.elprincipal);
-        if (drawer.isDrawerOpen(GravityCompat.START)){drawer.closeDrawer(GravityCompat.START);
-        }else{
-        super.onBackPressed();
+
+//==========================================  Funcionalidad para que regrese o se cierre el menu drawer despues de dar en el boton de atras=================
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.idDrawerLayout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
     }
-//=============================================================================================================
-
-    public boolean onNavigationItemSelected (MenuItem item){
-        int id = item.getItemId();
-
-        boolean FragmentTransantion = false;
-       // Fragment fragmentpersona1 = null;
-        FragmentManager fm = getSupportFragmentManager(); //llamada del suporte del gestor de fragment
+//==========================================================================================================================================================
 
 
-         if (id == R.id.idmenupersona1){
-             Fragment fragmentpersona1 = new Persona1Fragment();// Llama por las importaciones el fragment persona (Ojo el tipo de importacion deve ser igual en el mainactivity como en el fragment "import androidx.fragment.app.Fragment;")
-             FragmentTransantion = true;
-
-             Toast.makeText(this, "Persona 1", Toast.LENGTH_SHORT).show();
-            fm.beginTransaction().replace(R.id.fragmentContainerView,new Persona1Fragment()).commit();//opcion para hacer la transferencia
-        }
-        else
-            if (id == R.id.idmenupersona2){
-                Fragment fragmentpersona2 = new Persona2Fragment();// Llama por las importaciones el fragment persona (Ojo el tipo de importacion deve ser igual en el mainactivity como en el fragment "import androidx.fragment.app.Fragment;")
-                FragmentTransantion = true;
 
 
-                
-            Toast.makeText(this, "Persona 2", Toast.LENGTH_SHORT).show();
-            fm.beginTransaction().replace(R.id.elprincipal,new Persona2Fragment()).commit();
-        }
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.elprincipal);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-
-   
-
-
-    }
+}
 
--- app/src/main/java/com/miguelewv/menudesplegable/Persona2Fragment.java
+++ app/src/main/java/com/miguelewv/menudesplegable/Persona2Fragment.java
@@ -1,20 +1,62 @@
 package com.miguelewv.menudesplegable;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 
-public class Persona2Fragment extends Fragment {
+public class Persona2Fragment extends  Fragment {
 
-    @Override
+ /*   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_persona2, container, false);
+        return inflater.inflate(R.layout.fragment_persona2, container, false);}
+*/
+ /*       @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_persona2);
+
+
+        WebView webView = findViewById(R.id.idWebView);
+        webView.loadUrl(getString(R.string.google));
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setSupportZoom(true);
     }
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView webView = findViewById(R.id.idWebView);
+        if (keyCode == KeyEvent.KEYCODE_BACK && webView.canGoBack()){
+            webView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+*/
+
+
 }
